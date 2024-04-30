5bbbe0b0a6b0525f17700f8b1e5ae2cafb7a1eee
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-30 13:47:44 +0900

    同じ名前があちこちに分散しないようにするため

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,8 +1,10 @@
 import { ref } from 'vue'
 import * as localStorageHandlers from '../utils/localStorageHandlers.js'
 
+const TODOS_STORAGE_KEY = 'todos'
+
 export const useTodos = () => {
-  const todos = ref(localStorageHandlers.get('todos'))
+  const todos = ref(localStorageHandlers.get(TODOS_STORAGE_KEY))
 
   const addNewTodo = (newText) => {
     const freshTodoId = crypto.randomUUID()
@@ -12,20 +14,20 @@ export const useTodos = () => {
       content: newText
     }
 
-    localStorageHandlers.create('todos', newTodo)
+    localStorageHandlers.create(TODOS_STORAGE_KEY, newTodo)
 
     todos.value.push(newTodo)
   }
 
   const removeTodo = (targetId) => {
-    localStorageHandlers.remove('todos', targetId)
+    localStorageHandlers.remove(TODOS_STORAGE_KEY, targetId)
 
     const indexToRemove = todos.value.findIndex((todo) => todo.id === targetId)
     todos.value.splice(indexToRemove, 1)
   }
 
   const updateTodo = (targetId, updateText) => {
-    localStorageHandlers.update('todos', targetId, updateText)
+    localStorageHandlers.update(TODOS_STORAGE_KEY, targetId, updateText)
 
     todos.value = todos.value.map((todo) =>
       todo.id === targetId ? { ...todo, content: updateText } : todo
